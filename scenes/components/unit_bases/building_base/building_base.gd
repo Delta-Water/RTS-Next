2ef8d8467f9302b3b7c29021c40193e3bc6f4c7f
kirisauce <kirisauce@163.com>
2025-08-01 14:45:10 +0800

    feat: 为建筑加入建造状态和建造进度条

--- scenes/components/unit_bases/building_base/building_base.gd
+++ scenes/components/unit_bases/building_base/building_base.gd
@@ -1,15 +1,36 @@
 class_name BuildingBase
 extends StaticBody2D
 
-@export var preview_mode: bool = false :
+enum State {
+	Preview = 1,
+	Building = 2,
+	Ready = 3,
+}
+
+@export_enum("Preview:1", "Building:2", "Ready:3") var state: int = State.Building :
 	set(val):
-		display_container.modulate.a = 0.4 if val else 1.0
 		if collision_polygon:
-			collision_polygon.disabled = val
-			if val != preview_mode:
+			var is_preview: bool = state == State.Preview
+			if collision_polygon.disabled != is_preview:
 				var tree = get_tree()
 				if tree: tree.call_group("maps", "queue_bake")
-		preview_mode = val
+			collision_polygon.disabled = is_preview
+		state = val
+
+@export_group("Build")
+## 建造所需秒数
+@export_range(0.0, 60.0, 1.0, "or_greater") var build_time: float = 5.0
+
+## 正在建造的建造者数量
+@export var builder_num: int = 0
+
+## 当前建造进度，当超过 `build_time` 时视为建造完成。
+@export_range(0.0, 60.0, 0.1) var current_build_time: float = 0.0
+
+## 建造进度条边框颜色。
+@export var progress_bar_border_color: Color = Color(0.6, 0.73, 0.92)
+## 建造进度条填充颜色。
+@export var progress_bar_fill_color: Color = Color(0.4, 0.54, 0.78)
 
 @export_group("Size")
 ## 建筑的占地面积，只能使用整数坐标(单位为一个tile图格大小)。与碰撞体积无关。
@@ -41,11 +62,17 @@ extends StaticBody2D
 
 @onready var collision_polygon: CollisionPolygon2D = $CollisionPolygon
 @onready var display_container: Node2D = $DisplayContainer
+@onready var progress_bar: Node2D = $ProgressBar
+
+## 当前的建造进度，范围 [0,1]
+var current_build_progress: float = 0.0 :
+	get(): return clamp(current_build_time / build_time, 0.0, 1.0)
+	set(val): current_build_time = clamp(build_time * val, 0.0, build_time)
 
 func _ready() -> void:
 	_update_rect(get_rect())
 	collision_rect = collision_rect
-	preview_mode = preview_mode
+	state = state
 
 ## 通过节点组`maps`调用地图管理器更新节点位置。
 func _update_rect(old_rect: Rect2i) -> void:
@@ -66,5 +93,36 @@ func get_rect() -> Rect2i:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey && (event as InputEventKey).as_text_keycode() == "R" && (event as InputEventKey).is_pressed():
-		preview_mode = !preview_mode
-		print("preview_mode has switched to {0}".format([preview_mode]))
+		#state = State.Preview if state == State.Ready else State.Ready
+		state =  State.Building
+		current_build_progress = 0.0
+
+func _process(delta: float) -> void:
+	# 调整显示层透明度
+	var new_opacity: float = 0.4
+	match state:
+		State.Preview:
+			pass
+		State.Building:
+			new_opacity += current_build_progress * 0.6
+		State.Ready:
+			new_opacity = 1.0
+	display_container.modulate.a = new_opacity
+	
+	# 增加建造进度
+	if state == State.Building:
+		if current_build_time >= build_time:
+			state = State.Ready
+			queue_redraw()
+		elif builder_num > 0:
+			current_build_time += delta * builder_num
+			queue_redraw()
+
+func _draw() -> void:
+	if state == State.Building:
+		var global_size: Vector2 = display_container.position * 2.0
+		var pbar_rect := Rect2(global_size.x * 0.1, global_size.y + 10.0, global_size.x * 0.8, 5.0)
+		var pbar_fill_rect := pbar_rect
+		pbar_fill_rect.size.x *= current_build_progress
+		draw_rect(pbar_fill_rect, progress_bar_fill_color, true)
+		draw_rect(pbar_rect, progress_bar_border_color, false, 1)
